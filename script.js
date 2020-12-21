70b44c2983301d37d16af9950e031a6903f1cadf
elen1935 <elen1935@stud.kea.dk>
2020-12-21 12:49:19 +0100

    fixed categories an product page

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
+//cursor: pointer in the CSS
+
+let productPage = false;
+
 window.addEventListener('DOMContentLoaded', getData);
 
 const datalink = "http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine?per_page=100&_embed";
 
 function getData() {
-    //getCategories()
-    getFilters()
+  //getCategories();
 
-    const urlParams = new URLSearchParams(window.location.search);
-    console.log("URLSearchParams " + window.location);
-    const the_product_id = urlParams.get("product_id"); //getting the id from the URL
-    console.log(the_product_id);
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log("URLSearchParams " + window.location);
+  const the_product_id = urlParams.get("product_id");
+  const the_cat_id = urlParams.get("cat_id"); //getting the id from the URL
+  //  console.log(the_product_id);
+  console.log("cat: " + the_cat_id);
 
-    //routing in the script
-    if (the_product_id) {
-        fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine/" + the_product_id + "?per_page=100&_embed")
-            .then(res => res.json())
-            .then(showProduct) //skipping the forEach loop
-    } else if (!the_product_id && window.location.pathname == "/product.html") {
-        window.location.replace("wineshop.html");
-    } else {
-        fetch(datalink)
-            .then(res => res.json())
-            .then(handleData)
-    }
+  if (!the_product_id) {
+    getFilters();
+  }
+  //routing in the script
+  if (the_product_id) {
+    productPage = true;
+    fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine/" + the_product_id + "?per_page=100&_embed")
+      .then(res => res.json())
+      .then(showProduct) //skipping the forEach loop
+  } else if (the_cat_id) {
+    fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine?categories=" + the_cat_id + "&per_page=100&_embed")
+      .then(res => res.json())
+      .then(handleData)
+  } else if (!the_product_id && window.location.pathname == "/product.html") {
+    window.location.replace("wineshop.html");
+  } else {
+    fetch(datalink)
+      .then(res => res.json())
+      .then(handleData)
+  }
 
-   //hideProductInfoTabs();
+  //hideProductInfoTabs();
 }
+
 
 
 /* CATEGORIES FROM JONAS VIDEO */
@@ -47,84 +61,94 @@ function setUpCategories(catArray){
 
 /* CATEGORIES FROM LASSE VIDEO */
 function getFilters() {
-    fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories?parent=6")
-        .then(res => res.json())
-        .then(handleCategoryData)
+  fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories?parent=6")
+    .then(res => res.json())
+    .then(handleCategoryData)
 }
 
 function handleCategoryData(categories) {
-    //console.log("in handleCategory", categories)
-    categories.forEach(addCatLink);
+  //console.log("in handleCategory", categories)
+  categories.forEach(addCatLink);
 }
 
 function addCatLink(oneCategory) {
-    console.log(oneCategory);
-    const a = document.createElement('a');
-    a.textContent = oneCategory.name;
-    a.href = "wineshopcategory.html?cat_id=" + oneCategory.id;
-    console.log(a)
-    document.querySelector('.filterli').appendChild(a);
+  const templateCat = document.querySelector("template#categories_template").content;
+  const cloneCat = templateCat.cloneNode(true);
+  console.log(oneCategory);
+  const a = document.createElement('a');
+  a.textContent = oneCategory.name;
+  a.href = "wineshopcategory.html?cat_id=" + oneCategory.id;
+  console.log(a);
+  const li = cloneCat.querySelector('.filterli');
+  li.appendChild(a);
+  document.querySelector('.filterwrapper').appendChild(li);
 
-    /*
-    //CATEGORIES FROM TUTOR - AP PROJECT
-    //create another js just for categories and fetch from the link below
-    console.log("cat", oneCategory);
-    fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories/" + oneCategory.id)
-        .then(res => res.json())
-        .then((data) =>
-            let categoryData = data)
-    console.log(oneCategory);
+  /*
+  //CATEGORIES FROM TUTOR - AP PROJECT
+  //create another js just for categories and fetch from the link below
+  console.log("cat", oneCategory);
+  fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories/" + oneCategory.id)
+      .then(res => res.json())
+      .then((data) =>
+          let categoryData = data)
+  console.log(oneCategory);
 
-    const template = document.querySelector("#filterTemplate").content;
-    const copy = template.cloneNode(true);
-    console.log(copy)
-    copy.querySelector('a').textContent = oneCategory.name;
-    copy.querySelector('a').href = "category.html?cat_id=" + oneCategory.id;
-    document.querySelector("#filtercontainer").appendChild(copy);
-    */
+  const template = document.querySelector("#filterTemplate").content;
+  const copy = template.cloneNode(true);
+  console.log(copy)
+  copy.querySelector('a').textContent = oneCategory.name;
+  copy.querySelector('a').href = "category.html?cat_id=" + oneCategory.id;
+  document.querySelector("#filtercontainer").appendChild(copy);
+  */
 }
 
 
 function handleData(posts) {
-    console.log(posts)
-    posts.forEach(showProduct)
+  console.log(posts)
+  posts.forEach(showProduct)
 }
 
 function showProduct(product) {
-    //console.log(product)
-    const template = document.querySelector("template#product_template").content;
-    const clone = template.cloneNode(true);
+  //console.log(product)
+  const template = document.querySelector("template#product_template").content;
+  const clone = template.cloneNode(true);
 
-    /* GETTING DATA FOR WINESHOP PAGE */
-    clone.querySelector(".product_title").textContent = product.title.rendered;
-    clone.querySelector(".product_winery").textContent = product.winery;
-    clone.querySelector(".product_shortdescription").textContent = product.excerpt.rendered;
-    clone.querySelector(".product_price").textContent = product.price + " DKK";
-    clone.querySelector(".product_image").src = product._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+  /* GETTING DATA FOR WINESHOP PAGE */
+  clone.querySelector(".product_title").textContent = product.title.rendered;
+  clone.querySelector(".product_winery").textContent = product.winery;
+  clone.querySelector(".product_shortdescription").innerHTML = product.excerpt.rendered;
+  clone.querySelector(".product_price").textContent = parseInt(product.price) + " DKK";
+  //alert(product.price);
+  clone.querySelector(".product_image").src = product._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
 
-    /* GETTING DATA FOR PRODUCT PAGE */
-    /*clone.querySelector(".product_background").src = product.winebackground.guid;
+  /* GETTING DATA FOR PRODUCT PAGE */
+  if (productPage) {
+    clone.querySelector(".product_background").src = product.winebackground.guid;
     clone.querySelector(".volume").textContent = product.volume;
     clone.querySelector(".alcohol").textContent = product.alcohol_percentage;
     clone.querySelector(".tasteprofile_img").src = product.taste_profile.guid;
 
     clone.querySelector(".winery_logo").src = product.winery_logo.guid;
     clone.querySelector(".winery_name").textContent = product.winery;
-    clone.querySelector(".winery_description").textContent = product.winery_description;*/
-
-    const a = clone.querySelector('a');
-    if (a) {
-        a.href += product.id;
-    }
-
+    clone.querySelector(".winery_description").textContent = product.winery_description;
     const divProductLongDescription = clone.querySelector('.product_longdescription');
     if (divProductLongDescription) {
-        divProductLongDescription.innerHTML = product.content.rendered;
+      divProductLongDescription.innerHTML = product.content.rendered;
     }
 
-    document.querySelector("main").appendChild(clone);
+  }
 
-    //hideProductInfoTabs();
+  const a = clone.querySelector('a');
+  if (a) {
+    a.href += product.id;
+  }
+
+  document.querySelector("main").appendChild(clone);
+
+  if (productPage) {
+    hideProductInfoTabs();
+  }
+
 }
 
 
@@ -139,67 +163,67 @@ function showProduct(product) {
 
 //1
 function hideProductInfoTabs() {
-    console.log("hideProductInfoTabs");
-    document.querySelector(".description_info").classList.add("hide");
-    document.querySelector(".tasteprofile_info").classList.add("hide");
-    productDescriptionClicked();
-    productProfileClicked();
-    productWineInfoClicked();
-    }
+  console.log("hideProductInfoTabs");
+  document.querySelector(".description_info").classList.add("hide");
+  document.querySelector(".tasteprofile_info").classList.add("hide");
+  productDescriptionClicked();
+  productProfileClicked();
+  productWineInfoClicked();
+}
 
 //2
 function productDescriptionClicked() {
-    console.log("productDescriptionClicked");
-    document.querySelector(".description").addEventListener("click", showProductDescription);
+  console.log("productDescriptionClicked");
+  document.querySelector(".description").addEventListener("click", showProductDescription);
 }
 
 //3
 function showProductDescription() {
-    console.log("showProductDescription");
-    document.querySelector(".thewine_info_wrapper").classList.add("hide");
-    document.querySelector(".tasteprofile_info").classList.add("hide");
-    document.querySelector(".description_info").classList.remove("hide");
+  console.log("showProductDescription");
+  document.querySelector(".thewine_info_wrapper").classList.add("hide");
+  document.querySelector(".tasteprofile_info").classList.add("hide");
+  document.querySelector(".description_info").classList.remove("hide");
 }
 
 //4
 function productProfileClicked() {
-    console.log("productProfileClicked");
-    document.querySelector(".tasteprofile").addEventListener("click", showProductProfile);
+  console.log("productProfileClicked");
+  document.querySelector(".tasteprofile").addEventListener("click", showProductProfile);
 }
 
 //5
 function showProductProfile() {
-    console.log("showProductProfile");
-    document.querySelector(".description_info").classList.add("hide");
-    document.querySelector(".thewine_info_wrapper").classList.add("hide");
-    document.querySelector(".tasteprofile_info").classList.remove("hide");
+  console.log("showProductProfile");
+  document.querySelector(".description_info").classList.add("hide");
+  document.querySelector(".thewine_info_wrapper").classList.add("hide");
+  document.querySelector(".tasteprofile_info").classList.remove("hide");
 }
 
 //6
 function productWineInfoClicked() {
-    console.log("productWineInfoClicked");
-    document.querySelector(".thewine").addEventListener("click", showProductWineInfo);
+  console.log("productWineInfoClicked");
+  document.querySelector(".thewine").addEventListener("click", showProductWineInfo);
 }
 
 //7
 function showProductWineInfo() {
-    console.log("showProductWineInfo");
-    document.querySelector(".description_info").classList.add("hide");
-    document.querySelector(".tasteprofile_info").classList.add("hide");
-    document.querySelector(".thewine_info_wrapper").classList.remove("hide");
+  console.log("showProductWineInfo");
+  document.querySelector(".description_info").classList.add("hide");
+  document.querySelector(".tasteprofile_info").classList.add("hide");
+  document.querySelector(".thewine_info_wrapper").classList.remove("hide");
 }
 
 
 
-    /*const divProductShortDescription = copy.querySelector('.product_shortdescription');
-    if (divProductShortDescription) {
-        divProductShortDescription.innerHTML = product.excerpt.rendered;
-    }*/
+/*const divProductShortDescription = copy.querySelector('.product_shortdescription');
+if (divProductShortDescription) {
+    divProductShortDescription.innerHTML = product.excerpt.rendered;
+}*/
 
-    /*const divProductLongDescription = copy.querySelector('.product_longdescription');
-    if (divProductLongDescription) {
-        divProductLongDescription.innerHTML = product.content.rendered;
-    }*/
+/*const divProductLongDescription = copy.querySelector('.product_longdescription');
+if (divProductLongDescription) {
+    divProductLongDescription.innerHTML = product.content.rendered;
+}*/
 
 
 /*copy.querySelector(".content").innerHTML = post.content.rendered;*/
