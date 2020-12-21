2501176d83b6dfc00d341d3081f657a9d1784529
elen1935 <elen1935@stud.kea.dk>
2020-12-21 13:17:57 +0100

    cleanup js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const datalink = "http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine?per_page=10
 
 function getData() {
   //getCategories();
-
   const urlParams = new URLSearchParams(window.location.search);
   console.log("URLSearchParams " + window.location);
   const the_product_id = urlParams.get("product_id");
@@ -36,28 +35,8 @@ function getData() {
       .then(res => res.json())
       .then(handleData)
   }
-
   //hideProductInfoTabs();
 }
-
-
-
-/* CATEGORIES FROM JONAS VIDEO */
-/*function getCategories(){
-    fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories?parent=6")
-            .then(res => res.json())
-            .then(setUpCategories)
-}
-
-function setUpCategories(catArray){
-    const template = document.querySelector("template#categories_template").content;
-    const parentElement = document.querySelector("section");
-    catArray.forEach(cat=>{
-        const clone =template.cloneNode(true);
-        clone.querySelector("p").textContent=cat.name;
-        parentElement.appendChild(clone);
-    })
-}*/
 
 /* CATEGORIES FROM LASSE VIDEO */
 function getFilters() {
@@ -82,26 +61,7 @@ function addCatLink(oneCategory) {
   const li = cloneCat.querySelector('.filterli');
   li.appendChild(a);
   document.querySelector('.filterwrapper').appendChild(li);
-
-  /*
-  //CATEGORIES FROM TUTOR - AP PROJECT
-  //create another js just for categories and fetch from the link below
-  console.log("cat", oneCategory);
-  fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories/" + oneCategory.id)
-      .then(res => res.json())
-      .then((data) =>
-          let categoryData = data)
-  console.log(oneCategory);
-
-  const template = document.querySelector("#filterTemplate").content;
-  const copy = template.cloneNode(true);
-  console.log(copy)
-  copy.querySelector('a').textContent = oneCategory.name;
-  copy.querySelector('a').href = "category.html?cat_id=" + oneCategory.id;
-  document.querySelector("#filtercontainer").appendChild(copy);
-  */
 }
-
 
 function handleData(posts) {
   console.log(posts)
@@ -212,141 +172,3 @@ function showProductWineInfo() {
   document.querySelector(".tasteprofile_info").classList.add("hide");
   document.querySelector(".thewine_info_wrapper").classList.remove("hide");
 }
-
-
-
-/*const divProductShortDescription = copy.querySelector('.product_shortdescription');
-if (divProductShortDescription) {
-    divProductShortDescription.innerHTML = product.excerpt.rendered;
-}*/
-
-/*const divProductLongDescription = copy.querySelector('.product_longdescription');
-if (divProductLongDescription) {
-    divProductLongDescription.innerHTML = product.content.rendered;
-}*/
-
-
-/*copy.querySelector(".content").innerHTML = post.content.rendered;*/
-
-
-//window.addEventListener('DOMContentLoaded', getData);
-//
-//const datalink = "http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine?_embed";
-//
-//function getData() {
-//    getNav()
-//    const urlParams = new URLSearchParams(window.location.search);
-//    console.log("URLSearchParams " + window.location);
-//    const the_wine_id = urlParams.get("wine_id"); //getting the id from the URL
-//    console.log(the_wine_id);
-//
-//    //routing in the script
-//    if (wine_id) {
-//        fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine/" + the_wine_id + "?_embed")
-//            .then(res => res.json())
-//            .then(showChange) //skipping the forEach loop
-//    } else if (!the_wine_id && window.location.pathname == "/product.html") {
-//        window.location.replace("index.html");
-//    } else {
-//        fetch(datalink)
-//            .then(res => res.json())
-//            .then(handleData)
-//    }
-//}
-//
-////-----------------------------
-//
-//
-//function getNav() {
-//    fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories")
-//        .then(res => res.json())
-//        .then(data => handleCategoryNavData(data))
-//
-//}
-//
-//function handleCategoryNavData(categories) {
-//    console.log("in handleCategory", categories)
-//    categories.forEach(addNavLink);
-//}
-//
-//function addNavLink(oneCategory) {
-//    //create another js just for categories and fetch from the link below
-//
-//    console.log("cat", oneCategory);
-//    fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/categories/" + oneCategory.id)
-//        .then(res => res.json())
-//        .then((data) =>
-//            let categoryData = data)
-//    console.log(oneCategory);
-//
-//    const template = document.querySelector("#filterTemplate").content;
-//    const copy = template.cloneNode(true);
-//    console.log(copy)
-//    copy.querySelector('a').textContent = oneCategory.name;
-//    copy.querySelector('a').href = "category.html?cat_id=" + oneCategory.id;
-//    document.querySelector("#filtercontainer").appendChild(copy)
-//}
-
-
-
-//http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine?_embed"
-
-/*
-window.addEventListener('DOMContentLoaded', getData);
-
-const datalink = "http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine?_embed";
-
-function getData() {
-//    getNav()
-    const urlParams = new URLSearchParams(window.location.search);
-    console.log("URLSearchParams " + window.location);
-    const the_wine_id = urlParams.get("wine_id"); //getting the id from the URL
-    console.log(the_wine_id);
-
-    //routing in the script
-    if (the_wine_id) {
-        fetch("http://a-day.dk/semester-2-exam/wp-json/wp/v2/wine/" + the_wine_id + "?_embed")
-            .then(res => res.json())
-            .then(showWine) //skipping the forEach loop
-    } else if (!the_wine_id && window.location.pathname == "/product.html") {
-        window.location.replace("index.html");
-    } else {
-        fetch(datalink)
-            .then(res => res.json())
-            .then(handleData)
-    }
-}
-
-
-function handleData(data) {
-    //console.log(data);
-    data.forEach(showWine);
-}
-
-
-function showWine(wine) {
-//    console.log(wine)
-    console.log(wine.short_description)
-    const template = document.querySelector("#product_template").content;
-    const copy = template.cloneNode(true);
-    console.log(wine._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url)
-
-    copy.querySelector(".productlist_title").textContent = wine.title.rendered;
-    copy.querySelector(".productlist_winery").textContent = wine.title.rendered;
-    copy.querySelector(".shortdescription").textContent = wine.short_description;
-    copy.querySelector(".product_image").src = wine._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
-
-    const a = copy.querySelector('a');
-    if (a) {
-        a.href += wine.id;
-    }
-
-
-    const divChangeLongDescription = copy.querySelector('.longdescription');
-    if (divChangeLongDescription) {
-        divChangeLongDescription.innerHTML = wine.content.rendered;
-    }
-
-    document.querySelector("main").appendChild(copy);
-}
-*/
